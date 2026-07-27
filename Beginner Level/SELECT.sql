2bea2780269eb0dd68ddff9fafebe21107bd6738
Gaurav Singh <gauravbisht2803@gmail.com>
2026-07-27 23:17:22 +0530

    Add files via upload

---
+++ Beginner Level/SELECT.sql
@@ -0,0 +1,21 @@
+USE MyDatabase
+
+--This is a comment
+
+/*This is a comment
+is
+a
+comment*/
+
+SELECT * FROM customers
+
+select * from orders
+
+--1. Retrieve each customer's name , country and score
+
+select
+	first_name,
+	country,
+	score
+from customers
+
