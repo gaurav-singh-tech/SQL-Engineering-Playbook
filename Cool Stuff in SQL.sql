bbede4efdcb406c7cafbf21cb243cef92b41c37a
Gaurav Singh <gauravbisht2803@gmail.com>
2026-07-27 23:18:57 +0530

    Add files via upload

---
+++ Cool Stuff in SQL.sql
@@ -0,0 +1,25 @@
+--1. MULTIQUERIES--	
+
+SELECT *
+FROM customers
+
+SELECT *
+FROM orders
+
+
+--2. STATIC(FIXED) VALUES
+
+SELECT 123 as Static_value
+
+SELECT 'HELLO' as  STATIC_STRING
+
+
+SELECT
+id,
+first_name,
+'New__Customer' as Customer_type
+from customers
+
+
+--3. HIGHLIGHT & EXECUTE
+
