1f1978cabd442f676f5ee631d5be9f33a8c9f006
Gaurav Singh <gauravbisht2803@gmail.com>
2026-07-31 15:45:48 +0530

    Add files via upload

---
+++ Intermediate-level/Comparison Operatores.sql
@@ -0,0 +1,31 @@
+--** COMPARISON OPERATORES**--
+
+--Retrieve all customers from Germany
+
+Select *
+from customers
+where country= 'Germany'
+
+Select *
+from customers
+where country != 'Germany'
+
+
+Select * 
+from customers
+where score>500
+
+
+Select *
+from customers
+where score>=500
+
+
+
+select *
+from customers
+where score<500
+
+select *
+from customers
+where score<=500
