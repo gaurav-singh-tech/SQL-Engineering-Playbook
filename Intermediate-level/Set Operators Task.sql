4fb4eca09617b06c549695b336d425598c2130e7
Gaurav Singh <gauravbisht2803@gmail.com>
2026-08-07 14:54:53 +0530

    Add files via upload

---
+++ Intermediate-level/Set Operators Task.sql
@@ -0,0 +1,33 @@
+-- Orders data are stored in separate tables ( Orders and OrderArchieve)
+-- Combine all orders data into one report without duplicate
+
+
+select 
+      [ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+from sales.Orders
+
+UNION
+
+select
+      [ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+from Sales.OrdersArchive
